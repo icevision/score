8320660d6bc833e58c028ae17fbe6c8b08ab34f9
newpavlov <newpavlov@gmail.com>
2019-05-15 14:45:51 +0300

    update consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,9 +1,17 @@
 /// Number of classes
-pub const CLASSES_N: usize = 2;
+pub const CLASSES_N: usize = 10;
 /// Allowed sign classes.
 pub static ALLOWED_CLASSES: [&'static str; CLASSES_N] = [
-    "5.19",
+    "2.1",
+    "2.4",
     "3.1",
+    "3.24",
+    "3.27",
+    "4.1",
+    "4.2",
+    "5.19",
+    "5.20",
+    "8.22",
 ];
 /// False positive detection penalty
 pub const FP_PENALTY: f32 = 3.0;
